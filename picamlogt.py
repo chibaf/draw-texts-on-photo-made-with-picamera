21c3572bd2f5e4b9c9601f771f7a4397cd936f16
chibaf <chibaf@mac.com>
2024-06-23 07:03:06 +0900

    Add files via upload

--- picamlogt.py
+++ picamlogt.py
@@ -5,12 +5,13 @@ from PIL import Image, ImageDraw, ImageFont
 import time
 from datetime import date
 
+font = PIL.ImageFont.truetype("/usr/share/fonts/truetype/liberation2/LiberationMono-Regular.ttf",24)
 pc2 = Picamera2()
 pc2.start_preview( True )
 while True:
   today = date.today()
   t=time.localtime()
-  current_time=time.strftime("_H%H_M%M_S%S",t)
+  current_time=time.strftime(":%H:%M:%S",t)
   fn="photo_"+str(today)+current_time+".jpg"
   pc2.start()
   pc2.set_controls( {"AfMode" : controls.AfModeEnum.Continuous} )
@@ -18,6 +19,6 @@ while True:
   pc2.capture_file("tempp.jpg")
   img=Image.open("tempp.jpg")
   draw=ImageDraw.Draw(img)
-  draw.text((75, 50), current_time, 'red')
+  draw.text((20, 440), str(today)+current_time, 'red', font=font)
   img.save(fn)
   time.sleep(5)
